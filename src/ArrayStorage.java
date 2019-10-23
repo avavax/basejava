5c6ccbcba426dfa4fbccf62fe9ee4aeb4248c4d5
avavax <ip-media@yandex.ru>
2019-10-23 22:28:00 +0300

    Make ArrayStorage methods

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
@@ -5,26 +7,48 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        Arrays.fill(storage, null);
     }
 
-    void save(Resume r) {
+    void save(Resume resume) {
+        int position = this.size();
+        if (position != storage.length) {
+            storage[position] = resume;
+        }
     }
 
     Resume get(String uuid) {
+        for (Resume item:storage) {
+            if (item != null && item.uuid.equals(uuid)) {
+                return item;
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        int size = this.size();
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] != null && storage[i].uuid.equals(uuid)) {
+                storage[i] = storage[size - 1];
+                storage[size - 1] = null;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOf(storage, this.size());
     }
 
     int size() {
-        return 0;
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null) {
+                return i;
+            }
+        }
+        return storage.length;
     }
 }
