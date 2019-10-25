24bd5856c972ece926ccd33efa971b5c69afc627
avavax <ip-media@yandex.ru>
2019-10-25 20:54:46 +0300

    ArrayStorage v2

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -6,32 +6,39 @@ import java.util.Arrays;
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
+    private int size = 0;
+
     void clear() {
-        Arrays.fill(storage, null);
+        if (size != 0) {
+            Arrays.fill(storage, 0, size - 1, null);
+        }
+        size = 0;
     }
 
     void save(Resume resume) {
-        int position = this.size();
+        int position = size;
         if (position != storage.length) {
             storage[position] = resume;
+            size++;
         }
     }
 
     Resume get(String uuid) {
-        for (Resume item:storage) {
-            if (item != null && item.uuid.equals(uuid)) {
-                return item;
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                return storage[i];
             }
         }
         return null;
     }
 
     void delete(String uuid) {
-        int size = this.size();
-        for (int i = 0; i < storage.length; i++) {
-            if (storage[i] != null && storage[i].uuid.equals(uuid)) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(uuid)) {
                 storage[i] = storage[size - 1];
                 storage[size - 1] = null;
+                size--;
+                break;
             }
         }
     }
@@ -40,15 +47,10 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return Arrays.copyOf(storage, this.size());
+        return Arrays.copyOf(storage, size);
     }
 
     int size() {
-        for (int i = 0; i < storage.length; i++) {
-            if (storage[i] == null) {
-                return i;
-            }
-        }
-        return storage.length;
+        return size;
     }
 }
