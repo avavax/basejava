e2b557b9c8c0d1ccea87a803cc837145b512191a
avavax <ip-media@yandex.ru>
2019-11-26 22:09:47 +0300

    HW08 v2.2

--- src/com/basejava/webapp/model/Resume.java
+++ src/com/basejava/webapp/model/Resume.java
@@ -72,14 +72,17 @@ public class Resume {
         Resume resume = (Resume) o;
 
         if (!uuid.equals(resume.uuid)) return false;
-        return fullName.equals(resume.fullName);
+        if (!fullName.equals(resume.fullName)) return false;
+        if (!contacts.equals(resume.contacts)) return false;
+        return sections.equals(resume.sections);
     }
 
     @Override
     public int hashCode() {
         int result = uuid.hashCode();
         result = 31 * result + fullName.hashCode();
+        result = 31 * result + contacts.hashCode();
+        result = 31 * result + sections.hashCode();
         return result;
     }
-
 }
