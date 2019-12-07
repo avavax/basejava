07252057080c254b17904e537ef36cf1fb79c539
avavax <ip-media@yandex.ru>
2019-12-07 20:05:26 +0300

    HW12 stream

---
+++ src/com/basejava/webapp/MainStream.java
@@ -0,0 +1,31 @@
+package com.basejava.webapp;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class MainStream {
+    public static void main(String[] args) {
+
+        System.out.println(minValue(new int[] {4, 3, 2, 3}));
+
+        System.out.println(oddOrEven(Arrays.asList(4, 3, 2, 3, 7, 9)));
+    }
+
+    static int minValue(int[] values) {
+        IntStream stream = IntStream.of(values)
+                .distinct()
+                .sorted();
+        return stream.reduce(0, (acc, item) -> acc * 10 + item);
+    }
+
+    static List<Integer> oddOrEven(List<Integer> integers) {
+        Map<Boolean, List<Integer>> map = integers.stream()
+            .collect(Collectors.partitioningBy(item -> item % 2 == 0));
+        int sum = integers.stream()
+                .reduce(0, Integer::sum);
+        return map.get(sum % 2 == 0);
+    }
+}
